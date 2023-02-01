f0f55e074005e1229ff4da7fd54f2046ed612a33
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-02-01 19:56:59 +0500

    chore: update

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const app = require('express')();
-const { readdirSync } = require('fs');
-File = process.cwd();
+const app = require('express')()
+const { readdirSync } = require('fs-extra')
+path = process.cwd()
 
 app.get('/', (req, res) => {
-  res.send("Hello Oni-Chan")
+  res.send('Hello Oni-Chan')
 })
 
 app.get('/random', (req, res) => {
-const directory = '/directory/'
-const files = readdirSync(`.${directory}`)
-let randomFile = files[Math.floor(Math.random() * files.length)]
-res.sendFile(File + `${directory}${randomFile}`);
-});
+  const directory = '/directory/'
+  const files = readdirSync(`.${directory}`)
+  let randomFile = files[Math.floor(Math.random() * files.length)]
+  res.sendFile(path + directory + randomFile)
+})
 
+app.get('*', (req, res) => res.sendStatus(404))
 app.listen(8080, () => {
   console.log(`Port is Running 8080`)
-});
+})
